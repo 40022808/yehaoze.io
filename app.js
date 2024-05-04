e9feb920a59783dac4bbf458e77f019c456e4bc9
Haoze <70860816+40022808@users.noreply.github.com>
2024-05-04 16:32:57 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -4,23 +4,27 @@
     I-2-N
 */
 
-document.querySelector("#功能栏按钮_按钮_主页").onclick = function () {
+function 功能栏按钮_按钮_主页() {
     window.location.href = "index.html";
-};
-
-
-document.querySelector("#功能栏按钮_按钮_搜索").onclick = function () {
+}
+function 功能栏按钮_按钮_主页2() {
+    window.location.href = "index.html";
+}
+function 功能栏按钮_按钮_搜索() {
     window.location.href = "index2.html";
-};
+}
+function 功能栏按钮_按钮_搜索2() {
+    window.location.href = "index2.html";
+}
 
 
 
 const 搜索功能_确定 = document.querySelector(".搜索功能_确定")
 const input = document.querySelector("#input")
-
+const 搜索结果 = document.querySelector(".搜索结果")
 搜索功能_确定.addEventListener('click',()=>{
     let text = ""
     text = input.value
     input.value = ""
-    
+    搜索结果.innerHTML = "Nem található!"
 })
